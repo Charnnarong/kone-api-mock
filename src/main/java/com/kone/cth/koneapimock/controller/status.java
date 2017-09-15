fa83bfee0ed8f3bc8d6fb50c2bad73a096217a1d
Kone <charnnarong.chomthiang@gamil.com>
2017-09-15 13:32:04 +0700

    no message

--- src/main/java/com/kone/cth/koneapimock/controller/status.java
+++ src/main/java/com/kone/cth/koneapimock/controller/status.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class status {
 
 
-    @RequestMapping(value = "/status", produces = {"application/json", "application/xml"}, method = RequestMethod.GET)
+    @RequestMapping(path = "/status", produces = {"application/json", "application/xml"}, method = RequestMethod.GET)
     public String greeting() {
         return "{\"status\": \"OK\"}";
     }
