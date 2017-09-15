7054f25de16b18303b239aa13e91385489114ab2
Kone <charnnarong.chomthiang@gamil.com>
2017-09-15 13:32:23 +0700

    return status json

--- src/main/java/com/kone/cth/koneapimock/controller/status.java
+++ src/main/java/com/kone/cth/koneapimock/controller/status.java
@@ -1,15 +1,24 @@
 package com.kone.cth.koneapimock.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class status {
 
 
     @RequestMapping(path = "/status", produces = {"application/json", "application/xml"}, method = RequestMethod.GET)
-    public String greeting() {
-        return "{\"status\": \"OK\"}";
+    public ResponseEntity<Map<String, String>> greeting() {
+
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "OK");
+        return new ResponseEntity<Map<String, String>>(status,  HttpStatus.OK);
     }
 }
