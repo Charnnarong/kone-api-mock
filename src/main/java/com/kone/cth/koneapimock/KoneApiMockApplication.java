4a6d7f654db3e9fdd20ea29515d671e4b5da6df8
Kone <charnnarong.chomthiang@marlo.com.au>
2017-09-15 11:12:09 +0700

    Initial

---
+++ src/main/java/com/kone/cth/koneapimock/KoneApiMockApplication.java
@@ -0,0 +1,12 @@
+package com.kone.cth.koneapimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoneApiMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoneApiMockApplication.class, args);
+	}
+}
